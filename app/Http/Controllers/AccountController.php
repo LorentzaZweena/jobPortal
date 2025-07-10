1ee37b38dbf13d2ece90ce95304489f69a7ee1ce
Zweena Ariva <ariva02zweena@gmail.com>
2025-07-10 13:58:42 +0700

    User register logic

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AccountController extends Controller
+{
+    //ini adalah controller untuk mengatur akun pengguna
+    public function register()
+    {
+       return view('front.account.register');
+    }
+
+    //ini adalah fungsi untuk memproses register pengguna
+    public function processRegister(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if($validator->passes()){
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success', 'Registration successful. You can now login.');
+            return response()->json([
+                'status' => true,
+                'errors' => []
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+
+    public function login()
+    {
+       return view('front.account.login');
+    }
+}
