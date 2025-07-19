7aa6f43d71c1016d8dd1427f1d2ac44186d89158
Zweena Ariva <ariva02zweena@gmail.com>
2025-07-19 14:46:09 +0700

    Search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,21 +10,23 @@ class HomeController extends Controller
 {
     //ini adalah controller untuk halaman utama
     public function index() {
-        $categories = Category::where('status', 1)->orderBy('name', 'ASC')->take(8)->get();
+        $categories = Category::where('status', 1)->orderBy('name', 'ASC')->take(4)->get();
+        $newCategories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
         $featuredJobs = Job::where('status', 1)
                         ->orderBy('created_at', 'DESC')
                         ->with('jobType')
                         ->where('isFeatured', 1)->take(6)->get();
 
         $latestJobs = Job::where('status', 1)
-                        ->orderBy('created_at', 'DESC')
+                        ->orderBy('created_at', 'ASC')
                         ->with('jobType')
                         ->take(6)->get();
 
         return view('front.home', [
             'categories' => $categories,
             'featuredJobs' => $featuredJobs,
-            'latestJobs' => $latestJobs
+            'latestJobs' => $latestJobs,
+            'newCategories' => $newCategories,
         ]);
     }
 }
